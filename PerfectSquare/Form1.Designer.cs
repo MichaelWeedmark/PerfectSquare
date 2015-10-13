a2df224146d823521855dd2e051f5408739205b6
MichaelWeedmark <michael.weedmak@ocsbstudent.ca>
2015-10-13 13:11:53 -0400

    SquareRoot

---
+++ PerfectSquare/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PerfectSquare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudValue = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lsbList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudValue
+            // 
+            this.nudValue.Location = new System.Drawing.Point(82, 43);
+            this.nudValue.Name = "nudValue";
+            this.nudValue.Size = new System.Drawing.Size(120, 20);
+            this.nudValue.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(100, 84);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lsbList
+            // 
+            this.lsbList.FormattingEnabled = true;
+            this.lsbList.Location = new System.Drawing.Point(56, 118);
+            this.lsbList.Name = "lsbList";
+            this.lsbList.Size = new System.Drawing.Size(179, 251);
+            this.lsbList.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 381);
+            this.Controls.Add(this.lsbList);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.nudValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudValue;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lsbList;
+    }
+}
+
